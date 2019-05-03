c42ebc3bfcb06932651893ed4d7ae5b5f6f51e31
Júlia Wotzasek Pereira <37509043+JuliaWPereira@users.noreply.github.com>
2019-05-03 14:44:14 -0300

    Pilha de Dados
    Implementada com o template de dual-clock com dual port

---
+++ pilhaDeDados.v
@@ -0,0 +1,42 @@
+module pilhaDeDados
+#(parameter DATA_WIDTH=16, parameter ADDR_WIDTH=10)
+(
+	input [(DATA_WIDTH-1):0] data,
+	input reset,
+	input we, read_clock, write_clock,
+	output reg [(DATA_WIDTH-1):0] q,
+	output overflow, underflow, empty
+);
+	
+	// Declare the RAM variable
+	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+	reg [DATA_WIDTH-1:0] TOS;
+	
+	assign overflow = (TOS > 2**ADDR_WIDTH);
+	assign underflow = (TOS == 16'b1111111111111111);
+	assign empty = (TOS == 16'b00000000000000000);
+	
+	always @ (posedge write_clock)
+	begin
+		if(reset == 1)
+			TOS = 16'b00000000000000000;
+		// Write
+		else if (we)
+			begin
+				ram[TOS] <= data;
+				TOS = TOS + 16'b00000000000000001;
+			end
+		else
+			begin
+				TOS = TOS - 16'b00000000000000001;
+			end
+	end
+	
+	always @ (posedge read_clock)
+	begin
+		// Read 
+		q <= ram[TOS - 16'b00000000000000001];
+		
+	end
+	
+endmodule 
